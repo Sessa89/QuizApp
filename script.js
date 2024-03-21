3e1026d348073967706758f6469de87132065fe4
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-03-21 16:48:30 +0100

    added if-else-statement in order to stop functions when last question is reached

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let questions = [
     {
         "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",   
+        "answer_1": "Robbie Williams",
         "answer_2": "Lady Gaga",
         "answer_3": "Tim Berners-Lee",
         "answer_4": "Justin Bieber",
@@ -18,7 +18,7 @@ let questions = [
     },
     {
         "question": "Wie bindet man eine Website in eine Website ein?",
-        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",  
+        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
         "answer_2": "&lt;iframe&gt;",
         "answer_3": "&lt;frame&gt;",
         "answer_4": "&lt;frameset&gt;",
@@ -26,7 +26,7 @@ let questions = [
     },
     {
         "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
-        "answer_1": "readonly",   
+        "answer_1": "readonly",
         "answer_2": "max",
         "answer_3": "from",
         "answer_4": "spellcheck",
@@ -34,7 +34,7 @@ let questions = [
     },
     {
         "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
-        "answer_1": "a[title]{...}",   
+        "answer_1": "a[title]{...}",
         "answer_2": "a > title {...}",
         "answer_3": "a.title {...}",
         "answer_4": "a=title {...}",
@@ -42,7 +42,7 @@ let questions = [
     },
     {
         "question": "Wie definiert man in JavaScript eine Variable?",
-        "answer_1": "let 100 = rate;",  
+        "answer_1": "let 100 = rate;",
         "answer_2": "100 = let rate;",
         "answer_3": "rate = 100;",
         "answer_4": "let rate = 100;",
@@ -56,20 +56,26 @@ let currentQuestion = 0;
 // Definition der Funktion "Initialisierung"
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;      // Anzahl der Fragen entsprechend der Länge des Arrays "questions"
-    
+
     showQuestion();
 }
 
 // Definition der Funktion um die Frage anzuzeigen bzw. auszugeben
 function showQuestion() {
-    let question = questions[currentQuestion];  // Definition der Variable "question", hier: Array "questions" anhand Variable "currentQuestion" (hier = 0)
 
-    document.getElementById('question-number').innerHTML = currentQuestion + 1;
-    document.getElementById('questiontext').innerHTML = question['question'];   // Ausgabe der Frage im Feld mit ID = "questiontext" an der Stelle "0" des Arrays "questions"
-    document.getElementById('answer_1').innerHTML = question['answer_1'];       // Ausgabe der 1. Antwort im Feld mit ID = "answer_1" an der Stelle "0" des Arrays "questions"
-    document.getElementById('answer_2').innerHTML = question['answer_2'];       // Ausgabe der 2. Antwort im Feld mit ID = "answer_2" an der Stelle "0" des Arrays "questions"
-    document.getElementById('answer_3').innerHTML = question['answer_3'];       // Ausgabe der 3. Antwort im Feld mit ID = "answer_3" an der Stelle "0" des Arrays "questions"
-    document.getElementById('answer_4').innerHTML = question['answer_4'];       // Ausgabe der 4. Antwort im Feld mit ID = "answer_4" an der Stelle "0" des Arrays "questions"
+    if (currentQuestion >= questions.length) {      // if-else-Abfrage bzgl. der Arraylänge
+        // TODO: Show End Screen
+    } else {
+
+        let question = questions[currentQuestion];  // Definition der Variable "question", hier: Array "questions" anhand Variable "currentQuestion" (hier = 0)
+
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
+        document.getElementById('questiontext').innerHTML = question['question'];   // Ausgabe der Frage im Feld mit ID = "questiontext" an der Stelle "0" des Arrays "questions"
+        document.getElementById('answer_1').innerHTML = question['answer_1'];       // Ausgabe der 1. Antwort im Feld mit ID = "answer_1" an der Stelle "0" des Arrays "questions"
+        document.getElementById('answer_2').innerHTML = question['answer_2'];       // Ausgabe der 2. Antwort im Feld mit ID = "answer_2" an der Stelle "0" des Arrays "questions"
+        document.getElementById('answer_3').innerHTML = question['answer_3'];       // Ausgabe der 3. Antwort im Feld mit ID = "answer_3" an der Stelle "0" des Arrays "questions"
+        document.getElementById('answer_4').innerHTML = question['answer_4'];       // Ausgabe der 4. Antwort im Feld mit ID = "answer_4" an der Stelle "0" des Arrays "questions"
+    }
 }
 
 // Definition der Funktion bzgl. der ausgewählten Frage
@@ -83,7 +89,7 @@ function answer(selection) {    // "selection" entspricht hier dem Wert (Variabl
 
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
-    if(selectedQuestionNumber == question['right_answer']) {        // if-else-Abfrage => entspricht das letzte Zeichen der voran mitgegebenen Variablen dem Wert der richtigen Antwort, dann wird "richtig" in der Konsole ausgeben, ansonsten "falsch"
+    if (selectedQuestionNumber == question['right_answer']) {        // if-else-Abfrage => entspricht das letzte Zeichen der voran mitgegebenen Variablen dem Wert der richtigen Antwort, dann wird "richtig" in der Konsole ausgeben, ansonsten "falsch"
         console.log('Richtige Antwort!');
         document.getElementById(selection).parentNode.classList.add('bg-success');  // Eltern-Element bekommt die Klasse "bg-success" hinzugefügt (Container wird grün)
     } else {
